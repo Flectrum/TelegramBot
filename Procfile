4e0026a4e977217863f062b8851a3007dba05a44
Aleksandr Berdnikov <runer@hot.ee>
2022-01-20 22:49:05 +0200

    Added a Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -jar target/TelegramBot-1.1-SNAPSHOT.jar
--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -8,7 +8,6 @@ import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.Keyboard
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -101,7 +100,7 @@ public class Bot extends TelegramLongPollingBot {
         } catch (NumberFormatException e) {
             e.printStackTrace();
         }
-        return "0";
+        return "пока работаю только на вычисление... Введи количество рядов";
     }
     }
 
